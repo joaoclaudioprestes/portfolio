3808adf3a1fe0eb47d0d7826579990bce8f7e998
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2023-11-10 19:18:50 -0300

    :100:

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 const btnMenu = document.querySelector('#button-menu')
 const menu = document.querySelector('.menu')
+const html = document.querySelector('#html')
+const cxResultSkill = document.querySelector('.result-skill')
 
+
+
+html.addEventListener('mouseover', () =>{
+    cxResultSkill.textContent = 'O HTML é ...'
+});
 
 btnMenu.addEventListener('click', () => {
     menu.classList.toggle('hide')
